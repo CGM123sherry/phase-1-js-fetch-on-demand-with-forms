50e732731f9bc6fc7514aaa1bba27ad0b4a19c41
CGM123sherry <g.christinemurithi@gmail.com>
2024-07-10 14:47:35 +0300

    completed fetch lab on forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
-}
+    //declare a function
+    const inputForm = document.querySelector("form");
+//add event listener
+  inputForm.addEventListener("submit", (event) => {
+    //prevent reload
+    event.preventDefault();
+    const input = document.querySelector("input#searchByID");
+    console.log(event);
 
+    //fetchdata
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+  });
+});
+};
 document.addEventListener('DOMContentLoaded', init);
+
+
